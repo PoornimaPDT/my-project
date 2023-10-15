dced4d20c23b425bd4f1970ee7667d30a5f89f98
PoornimaPDT <poornimapdt.20@uom.lk>
2023-10-15 19:05:12 +0530

    print the summation of two numbers

--- Test2.py
+++ Test2.py
@@ -4,6 +4,10 @@ class Car:
     def __init__(self, brand):
         self.brand = brand
 
+
 def add_numbers(a, b):
     """Returns the sum of two numbers."""
     return a + b
+
+
+print(add_numbers(15, 5))
